694a700cc087bed37d0aba9ecc7c8354a2f7dc0e
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-02-21 15:16:54 +0000

    delete a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,6 +27,13 @@ class RestaurantsController < ApplicationController
     redirect_to restaurants_path
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to restaurants_path
+  end
+
   private
   def restaurant_params
     params.require(:restaurant).permit(:name, :description)
